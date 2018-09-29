90c0289d2a5a1f71eb5aaadfa44edcc72ecee004
Junior Mendes <vilmarmendesjunior@hotmail.com>
2018-09-29 14:23:07 -0300

    Projeto criado

---
+++ src/main/java/com/lifecare/LifecareWebApplication.java
@@ -0,0 +1,12 @@
+package com.lifecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifecareWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifecareWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lifecare/LifecareWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lifecare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LifecareWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
