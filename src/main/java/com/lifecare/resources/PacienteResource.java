62b734db5de6ae50f4bdd04991aaedd27d8d6c0e
Junior Mendes <vilmarmendesjunior@hotmail.com>
2018-10-07 16:10:31 -0300

    Insert, update e delete para Paciente

--- src/main/java/com/lifecare/resources/PacienteResource.java
+++ src/main/java/com/lifecare/resources/PacienteResource.java
@@ -3,6 +3,7 @@ package com.lifecare.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,19 @@ public class PacienteResource {
 	public ResponseEntity<Paciente> find(@PathVariable Integer id){
 		Paciente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Paciente obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/lifecare/services/PacienteService.java
+++ src/main/java/com/lifecare/services/PacienteService.java
@@ -1,10 +1,12 @@
 package com.lifecare.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lifecare.domain.Paciente;
 import com.lifecare.repositories.PacienteRepository;
+import com.lifecare.services.exceptions.DataIntegrityException;
 import com.lifecare.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +21,27 @@ public class PacienteService {
 			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + " Tipo: " + Paciente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Paciente insert(Paciente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Paciente update(Paciente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um objeto com entidades associadas");
+		}
+			
 	}
 	
 }
